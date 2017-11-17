f0e7f07c832d4e569ec3c8416695dd97cca9bb1f
Rajdattpawar <rajpawar02@gmail.com>
2017-11-17 15:26:25 +0530

    Update add_user.sh

--- add_user.sh
+++ add_user.sh
@@ -5,8 +5,8 @@
 
 # Added because 12.4 beta uses adm instead of admin.
 addgroup --system admin
-password='rpawar01'
-for user in rpawar01
+password='USERNAME'
+for user in USERNAME
 
 do
         output=`id $user > /dev/null 2>&1`
@@ -19,8 +19,8 @@ do
 				su -c "mkdir --mode=700 /home/$user/.ssh" $user
 				chage -M 90 $user
 				chage -I -1 $user
-				if [ $user = "rpawar01" ]; then
-						su -c "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAk87yx6dmwFDN4gw0rg76tGaEiQECu1wk5TjPa4OC11TziIZI0gF24rpUSKUq7A+sgj09gwGEyEzRYnH1s+aqyabR4Yfb563hCMX4XnsRMqAqUOH0PEoLjbv52xybeuZwr45Rm9hWO34XgdwN4li3SBT9tvXk7kHHYiVJ93kziUc= rpawar01' > /home/$user/.ssh/authorized_keys" $user
+				if [ $user = "USERNAME" ]; then
+						su -c "echo 'ssh-rsa ADDYOURKEYHERE USERNAME' > /home/$user/.ssh/authorized_keys" $user
 				fi						
 				if [ ! -f /home/$user/.bashrc ]; then
 						su -c "echo \"export PS1='\[\e[0;35m\]\u@\h:\w>\[\e[m\]'\" > /home/$user/.bashrc" $user
